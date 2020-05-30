87b6f2772317c843bc2daf2ff81a75728cc2a8bf
Muhamar <amararrasyid@gmail.com>
2020-05-30 20:41:58 +0700

    Tugas 2 PHP
    Mengerjakan Tugas Niomic

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$angka_1 = 36;
+$pekerja = 12;
+$pekerja_2 = $pekerja * 2;
+
+$hasil = $angka_1 / 2;
+echo "$angka_1 Hari = $pekerja Pekerja <br>";
+echo "X hari = $pekerja_2 Pekerja <br>";
+echo "X = $hasil Hari";
+?>
---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php
+$angka_pertama = 80000;
+$angka_kedua = 50000;
+
+$hasil = $angka_pertama - $angka_kedua;
+
+
+echo "Jual = $angka_pertama <br>";
+echo "Beli = $angka_kedua <br>";
+echo "Keuntungan = ....? <br>";
+echo "Jawaban : $hasil";
+
+?>
